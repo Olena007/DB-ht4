c928515262b521d341c34368749a5d16a9b6cd90
lena <llogvinovaaaa@gmail.com>
2022-07-12 15:26:41 +0200

    codefirst

---
+++ DB-codeFirst/DB-codeFirst/DbModels/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_codeFirst.DbModels
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Request { get; set; }
+
+        public List<Project> Clients { get; set; } = new List<Project>();
+    }
+}
---
+++ DB-codeFirst/DB-codeFirst/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DB_codeFirst.DbModels;
+
+namespace DB_codeFirst
+{
+    public sealed class Program
+    {
+        internal static void Main(string[] args)
+        {
+            using (ApplicationContext context = new ApplicationContextFactory().CreateDbContext(Array.Empty<string>()))
+            {
+                context.Database.EnsureCreated();
+
+                var firstOffice = context.Office.SingleOrDefault(x => x.OfficeId == 1);
+
+                if (firstOffice is not null)
+                {
+                    firstOffice.Title = "lalala";
+                }
+
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("done");
+        } 
+    }
+}
+
+
